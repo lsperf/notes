4955fc20b8b2ff25c3925a9c40ca4700f9ee07ba
Levon <saryan_levon@yahoo.com>
2021-06-19 13:30:47 -0700

    search and replace tasks

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# notes
+# This repo represents search and replace tasks added to git project https://github.com/AnomalyInnovations/serverless-stack-demo-client
+
+1 - Filter notes: Implement functionality on the homescreen that allows the user to find notes containing a specific spring.
+2 - Bulk find-and-replace: Implement functionality that allows the user to replace all occurrences of a given string across all saved notes, with a new string supplied by the user. For example, I should be able to replace "Sam" with "Levon" in all of my saved notes with one button press.
+3 - Loading indicator - make it so that some kind of indicator appears while all the notes are being retrieved from the API for the homescreen, when an individual note is being loaded for viewing, and when find-and-replace is in progress.
+
+# Serverless Stack Demo React App
+
+The [Serverless Stack Guide](http://serverless-stack.com) is a free comprehensive resource to creating full-stack serverless applications. We create a [note taking app](http://demo2.serverless-stack.com) from scratch.
+
+This repo is for the frontend React app that we build over the course of the tutorial. You can find the repo for the backend serverless API [here](https://github.com/AnomalyInnovations/serverless-stack-demo-api). And the repo for the tutorial [here](https://github.com/AnomalyInnovations/serverless-stack-com).
+
+#### Steps
+
+To support the different chapters and steps of the tutorial; we use branches to represent the project codebase at the various points. Here is an index of the various chapters and branches in order.
+
+- [Initialize the Frontend Repo](../../tree/initialize-the-frontend-repo)
+- [Configure AWS Amplify](../../tree/configure-aws-amplify)
+- [Redirect on Login](../../tree/redirect-on-login)
+- [Create a Build Script](../../tree/create-a-build-script)
+
+#### Usage
+
+This project is created using [Create React App](https://github.com/facebookincubator/create-react-app).
+
+To use this repo locally, start by cloning it and installing the NPM packages.
+
+``` bash
+$ git clone https://github.com/AnomalyInnovations/serverless-stack-demo-client
+$ npm install
+```
+
+Run it locally.
+
+``` bash
+$ npm run start
+```
+
+---
+
+This repo is maintained by [Serverless Stack](https://serverless-stack.com).
+
+[Email]: mailto:hello@serverless-stack.com
---
+++ src/containers/Home.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
+import { BsPencilSquare } from "react-icons/bs";
+import ListGroup from "react-bootstrap/ListGroup";
+import { LinkContainer } from "react-router-bootstrap";
+import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
+import "./Home.css";
+import _ from "lodash";
+
+export default function Home() {
+  const [notes, setNotes] = useState([]);
+  const [filteredNotes, setFilteredNotes] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+  const [replaceText, setReplaceText] = useState("");
+  
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        //const notes = ["Hello Sam!", "The note for Sam", "Note #3"]//temp for test
+        setNotes(notes);
+        setFilteredNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+
+    
+      
+  }, [filter]);
+  
+  function loadNotes() {
+    return API.get("notes", "/notes");
+  }
+
+  function renderNotesList(notes) {
+    return (
+      <>
+        {isLoading && <div style={{
+            width: "100%",
+            borderRadius: 10,
+            border: "3px dashed green",
+            textAlign: "center",
+            marginTop: "10px",
+            marginBottom: "10px"
+          }}> Loading ... </div> }
+        {!isLoading && <>
+          Search (Results will be filter on typing. Case Insensitive.): 
+          <input type="text" value={filter} 
+            onChange={(e)=>{
+              setIsLoading(true);
+              setFilter(e.target.value)
+              setFilteredNotes(_.cloneDeep(notes).filter(n => 
+                n.content.toLowerCase().indexOf(e.target.value.toLowerCase())!==-1 ))
+              setIsLoading(false);
+            }} 
+          />
+          Replace (search text above will be replaced with this value. Case Insensitive. ): 
+          <input type="text" value={replaceText} 
+            onChange={(e)=>{
+              setReplaceText(e.target.value)
+            }} 
+          />
+          <div
+            style={{
+              width: 270,
+              height: 26,
+              borderRadius: 5,
+              border: "1px solid grey",
+              textAlign: "center",
+              marginTop: "10px",
+              marginBottom: "10px",
+              cursor: "pointer"
+            }}
+            onClick={(e)=>{
+              setIsLoading(true);
+              let nt = _.cloneDeep(notes);
+              console.log(filter, replaceText)
+              nt.forEach(n => n.content = n.content.split(filter).join(replaceText));
+              setFilteredNotes(nt);
+              setIsLoading(false);
+            }} 
+          > REPLACE SEARCH TEXT IN NOTES </div>
+        </>}
+
+        <LinkContainer to="/notes/new">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+            <BsPencilSquare size={17} />
+            <span className="ml-2 font-weight-bold">Create a new note</span>
+            
+          </ListGroup.Item>
+        </LinkContainer>
+        {filteredNotes.map(({ noteId, content, createdAt }) => (
+          <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+            <ListGroup.Item action>
+              <span className="font-weight-bold">
+                {content.trim().split("\n")[0]}
+              </span>
+              <br />
+              <span className="text-muted">
+                Created: {new Date(createdAt).toLocaleString()}
+              </span>
+            </ListGroup.Item>
+          </LinkContainer>
+        ))}
+      </>
+    );
+  }
+
+  function renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p className="text-muted">A simple note taking app</p>
+        <div className="pt-3">
+          <Link to="/login" className="btn btn-info btn-lg mr-3">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  }
+  
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
+    </div>
+  );
+}
